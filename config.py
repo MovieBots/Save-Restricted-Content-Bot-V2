e3ab7b42dee9bf191c095da4e2c417a08b391e16
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-09-25 14:24:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "29236719"))
+API_HASH = getenv("API_HASH", "1ccf1bd0a86af974e3210a55f662c062")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "893383574").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://keshavptdr98:D8lbdQUW4euV07l4@cluster0.dok926y.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP = getenv("LOG_GROUP", "-1002074744533")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
